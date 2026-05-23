f757000156d65b3cd318a549f4d5a1827c9b296b
ADJSAMIRA <samira.adjenef@univ-constantine2.dz>
2026-05-23 13:45:07 +0100

    Delete Backend_Smart_Residence_PFE/smart-residence-backend directory

--- Backend_Smart_Residence_PFE/smart-residence-backend/routes/maintenanceRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const maintenanceController = require('../controllers/maintenanceStaffController');
-
-router.get('/home/:id', maintenanceController.getMaintenanceHome);
-router.get('/tasks/:id', maintenanceController.getMaintenanceTasks);
-router.put('/tasks/:taskId/update', maintenanceController.updateMaintenanceTask);
-module.exports = router;
--- Backend_Smart_Residence_PFE/smart-residence-backend/routes/visitorRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const visitorController = require('../controllers/visitorController');
-
-router.post('/register', visitorController.registerGuest);
-router.get('/my-guests/:apartmentId', visitorController.getMyGuests);
-router.get('/admin/all-guests', visitorController.adminGetAllGuests);
-module.exports = router;
